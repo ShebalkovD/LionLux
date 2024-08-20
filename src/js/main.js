e78dc2349f0abe6e9fda553dde7ce5c3cdb65c57
DenisS <shebalkovdenis.wk@gmail.com>
2024-08-20 22:26:21 +0300

    каталог в хедере

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,6 @@
+let nav = document.querySelector('#nav')
+let catalog = document.querySelector('#header_catalog')
+
 let options = {
     root: null,
     rootMargin: "0px",
@@ -5,12 +8,28 @@ let options = {
 };
 
 function change_nav(entries, observer) {
-    let nav = document.querySelector('#nav')
     const [entry] = entries;
-    if (!entry.isIntersecting) nav.classList.add("nav_active");
-    if (entry.isIntersecting) nav.classList.remove("nav_active");
+    if (!entry.isIntersecting) nav.classList.add("nav_fixed");
+
+    if (entry.isIntersecting && !catalog.classList.contains('active')){
+        nav.classList.remove("nav_fixed");
+    }
 }
 
 let nav_observer = new IntersectionObserver( change_nav, options);
 let target = document.querySelector(".header_observertrigger");
 nav_observer.observe(target)
+
+const nav_catalog_btn = document.querySelector('#nav_catalog_button');
+
+nav_catalog_btn.addEventListener('click', function() {
+    let posTop = window.scrollY;
+    
+    if (catalog.classList.contains('active') && posTop === 0) {
+        nav.classList.remove('nav_fixed')
+    }else {
+        nav.classList.add('nav_fixed')
+    }
+    
+    catalog.classList.toggle('active')
+})
