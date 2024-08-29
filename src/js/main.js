6b6b7f75b171b2a5234757142ca8bff57ec44d57
DenisS <shebalkovdenis.wk@gmail.com>
2024-08-29 17:42:34 +0300

    мобильное меню

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,6 @@
 let nav = document.querySelector('#nav')
 let catalog = document.querySelector('#header_catalog')
-
+let body = document.querySelector("body")
 let options = {
     root: null,
     rootMargin: "0px",
@@ -32,4 +32,24 @@ nav_catalog_btn.addEventListener('click', function() {
     }
     
     catalog.classList.toggle('active')
+})
+
+let burger_btn = document.querySelector("#nav_burger")
+let burger_menu = document.querySelector("#burger_menu")
+let burger_close_btn = document.querySelector("#burger_menu_close")
+
+burger_btn.addEventListener("click", function() {
+    burger_menu.style.display = "flex";
+    body.classList.add("no_scroll")
+    setTimeout(function(){
+        burger_menu.classList.add("burger_menu-active")
+    }, 100)
+})
+
+burger_close_btn.addEventListener("click", function() {
+    burger_menu.classList.remove("burger_menu-active")
+    body.classList.remove("no_scroll")
+    setTimeout(function(){
+        burger_menu.style.display = "none";
+    }, 200)
 })
