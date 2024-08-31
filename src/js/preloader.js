1c59bdd7b9eaa8ed5f184c51bebb7bf686c7df2c
DShebalkov <shebalkovdenis.wk@gmail.com>
2024-08-31 21:17:05 +0300

    loader

---
+++ src/js/preloader.js
@@ -0,0 +1,10 @@
+// let preloader = document.querySelector("#preloader")
+
+// window.onload = (e) => {
+//     preloader.classList.add('hide')
+    
+//     setTimeout(function() {
+//         preloader.style.display = 'none'
+
+//     }, 300)
+// }
